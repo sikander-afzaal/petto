f0f462c14e0083c7aa6d546531e846f38fc8a145
sikander-afzaal <sikander.aaa@gmail.com>
2022-08-28 20:06:17 +0500

    website done

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -6,13 +6,14 @@ function Header({ setModal, setModalName }) {
   const [header, setHeader] = useState(false);
   return (
     <div className="w-full flex justify-center items-center">
-      <div className="flex justify-between items-center w-full max-w-[865px] h-[120px] px-5">
+      <div className="flex lg:justify-start justify-between lg:gap-[120px] items-center w-full max-w-[900px] h-[120px] px-5">
         <FontAwesomeIcon
           onClick={() => setHeader((prev) => !prev)}
           className="text-white text-2xl relative z-30 md:hidden block"
           icon={header ? faXmark : faBars}
         />
         <div
+          onClick={() => setHeader(false)}
           className={`md:hidden ${
             header ? "block" : "hidden"
           } fixed top-0 left-0 w-screen h-screen z-[19] bg-black opacity-60`}
@@ -20,7 +21,7 @@ function Header({ setModal, setModalName }) {
         <div
           className={`transition-all md:flex-row flex-col md:bg-transparent bg-darkOrange z-20 md:static fixed top-0 ${
             header ? "left-0" : "-left-[600px]"
-          } h-screen md:h-auto md:max-w-none max-w-[400px] w-full md:w-auto flex justify-center items-center gap-6`}
+          } h-screen md:h-auto md:max-w-none max-w-[400px] w-full md:w-auto flex justify-center items-center gap-[30px]`}
         >
           <p
             onClick={() => {
